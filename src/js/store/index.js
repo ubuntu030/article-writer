4089edf87adc449ae58ea555eeb2c3409a1b1544
ubuntu030 <ubuntu030@gmail.com>
2020-12-18 13:58:50 +0800

    code for enable Redux Dev Tools

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,10 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
 import { forbiddenWordsMiddleware } from "../middleware";
 
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
-	rootReducer,
-	applyMiddleware(forbiddenWordsMiddleware)
+  rootReducer,
+  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware))
 );
 
 export default store;
