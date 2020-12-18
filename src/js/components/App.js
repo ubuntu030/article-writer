db21bdf4e61eb4def2c68b411ddd3baeb21ad2e0
ubuntu030 <ubuntu030@gmail.com>
2020-12-18 13:53:51 +0800

    表單元件

---
+++ src/js/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import List from "./List";
+import Form from "./Form";
+
+const App = () => (
+	<div>
+		<h2>Articles</h2>
+		<List />
+		<h2>Add a new article</h2>
+		<Form />
+	</div>
+);
+
+export default App;
